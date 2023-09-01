c67d2971f2d3f8e909e9860510a462c4b9dfe8ac
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-01 01:07:57 -0700

    main application

---
+++ src/test/java/com/sfg/springframework6/section2/spring6di/Spring6DiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.springframework6.section2.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
