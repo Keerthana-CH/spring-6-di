498ff9d45c50d9a320fcb0b4dbb39604cbc1c771
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-01 05:06:31 -0700

    2 different ways to test

--- src/test/java/com/sfg/springframework6/section2/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/sfg/springframework6/section2/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,32 @@
 package com.sfg.springframework6.section2.spring6di;
 
+import com.sfg.springframework6.section2.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testMyCntrlrThroughApplCntxBean(){
+        MyController myController = applicationContext.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testMyCntrlrBeanThroughAutowiring(){
+        System.out.println(myController.sayHello());
+    }
 
     @Test
     void contextLoads() {
